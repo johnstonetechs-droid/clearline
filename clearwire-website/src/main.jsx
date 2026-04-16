a994d74c999ff47f81d59cfc2d30a628b4db37cb
johnstonetechs-droid <johnstonetechs@gmail.com>
2026-04-15 21:15:40 -0400

    Update main.jsx

--- clearwire-website/src/main.jsx
+++ clearwire-website/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './ClearLineWebsite.jsx'
+import App from './ClearWireWebsite.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
